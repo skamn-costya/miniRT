454d09602f76dea4efb73b02f238e3db1320162c
ksorokol <ksorokol@student.42prague.com>
2025-03-04 13:00:06 +0100

    Draw box

--- src/graphics/3D/ksx_draw_box.c
+++ src/graphics/3D/ksx_draw_box.c
@@ -6,7 +6,7 @@
 /*   By: ksorokol <ksorokol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 13:38:25 by ksorokol          #+#    #+#             */
-/*   Updated: 2025/03/04 00:49:56 by ksorokol         ###   ########.fr       */
+/*   Updated: 2025/03/04 12:59:49 by ksorokol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,25 @@ void	ksx_draw_box(t_object *p_object,
 {
 	uint32_t	idx;
 	t_box		**pp_box;
+	t_triangle	*p_tri;
+	t_pixel		pixel[3];
 
-	// if (!p_object->pp_box || !p_object->pp_box[0])
-	// 	return ;
 	pp_box = p_object->pp_box;
 	while (pp_box && *pp_box)
 	{
 		idx = 0;
 		while (idx < 12)
 		{
-			(void) p_image;
-			(void) p_camera;
-			// ksx_draw_tri(&(*pp_box)->tris[idx], p_image, p_camera);
+			p_tri = &(*pp_box)->tris[idx];
+			pixel[0] = ksx_draw_get_pixel(p_camera,
+					&p_tri->p_ver1->cp, 0x88888888);
+			pixel[1] = ksx_draw_get_pixel(p_camera,
+					&p_tri->p_ver2->cp, 0x88888888);
+			pixel[2] = ksx_draw_get_pixel(p_camera,
+					&p_tri->p_ver3->cp, 0x88888888);
+			ksx_line(p_image, &pixel[0], &pixel[1]);
+			ksx_line(p_image, &pixel[1], &pixel[2]);
+			ksx_line(p_image, &pixel[2], &pixel[0]);
 			idx++;
 		}
 		pp_box++;
