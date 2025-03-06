c5519951c1090eb32f4ffe8d59b8232dfd9a34df
ksorokol <ksorokol@student.42prague.com>
2025-03-06 19:10:44 +0100

    norm vectors for objects from file

--- src/graphics/obj_file/ksx_obj_file.c
+++ src/graphics/obj_file/ksx_obj_file.c
@@ -6,7 +6,7 @@
 /*   By: ksorokol <ksorokol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:26:20 by ksorokol          #+#    #+#             */
-/*   Updated: 2025/03/06 14:04:57 by ksorokol         ###   ########.fr       */
+/*   Updated: 2025/03/06 19:08:18 by ksorokol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 #include "parser.h"
 #include <fcntl.h>
 #include <stdio.h>
-
-static int	get_f_split(char *p_str);
 
 void	read_of(t_world *p_world, char *file_name)
 {
@@ -45,10 +43,10 @@ void	read_of(t_world *p_world, char *file_name)
 		// if (!ft_strcmp(p_str[0], OBJ_G))
 		if (!ft_strcmp(p_fline->words[0], OBJ_V))
 			get_v_of(p_fline, p_object, &i[0]);
-		if (!ft_strcmp(p_fline->words[0], OBJ_F))
-			get_f_of(p_fline, p_object, &i[1]);
 		if (!ft_strcmp(p_fline->words[0], OBJ_VN))
 			get_vn_of(p_fline, p_object, &i[1]);
+		if (!ft_strcmp(p_fline->words[0], OBJ_F))
+			get_f_of(p_fline, p_object, &i[2]);
 		p_list[1] = p_list[1]->next;
 	}
 	ft_lstclear(&p_list[0], &free_t_fline);
@@ -72,22 +70,24 @@ t_vertex	*get_v_of(t_fline *p_fline, t_object *p_object, int *p_idx)
 	return (p_object->pp_vrtx[*p_idx]);
 }
 
+static void	get_f_split(char *p_str, int *p_i);
+
 t_triangle	*get_f_of(t_fline *p_fline, t_object *p_object, int *p_idx)
 {
-	int	i[4];
+	int	i[4][3];
 
-	i[0] = get_f_split(p_fline->words[1]);
-	i[1] = get_f_split(p_fline->words[2]);
-	i[2] = get_f_split(p_fline->words[3]);
-	if (i[0] < 0 || i[1] < 0 || i[2] < 0)
+	get_f_split(p_fline->words[1], i[0]);
+	get_f_split(p_fline->words[2], i[1]);
+	get_f_split(p_fline->words[3], i[2]);
+	if (i[0][0] < 0 || i[1][0] < 0 || i[2][0] < 0)
 		return (NULL);
 	if (!p_object->pp_tri)
 		p_object->pp_tri = ksx_obj_new_tris(1);
 	else
 		ksx_obj_add_tris(p_object, 1);
-	p_object->pp_tri[*p_idx]->p_ver1 = p_object->pp_vrtx[i[0] - 1];
-	p_object->pp_tri[*p_idx]->p_ver2 = p_object->pp_vrtx[i[1] - 1];
-	p_object->pp_tri[*p_idx]->p_ver3 = p_object->pp_vrtx[i[2] - 1];
+	p_object->pp_tri[*p_idx]->p_ver1 = p_object->pp_vrtx[i[0][0] - 1];
+	p_object->pp_tri[*p_idx]->p_ver2 = p_object->pp_vrtx[i[1][0] - 1];
+	p_object->pp_tri[*p_idx]->p_ver3 = p_object->pp_vrtx[i[2][0] - 1];
 	p_object->pp_tri[*p_idx]->color.mlx_color = 0xffffffff;
 	// if ((p_fline->words[4] && !is_i_number(p_fline->words[4])))
 	// {
@@ -98,29 +98,46 @@ t_triangle	*get_f_of(t_fline *p_fline, t_object *p_object, int *p_idx)
 	// 	p_object->pp_tri[*p_idx]->p_ver3 = p_object->pp_vrtx[ft_atoi(p_fline->words[4]) - 1];
 	// 	p_object->pp_tri[*p_idx]->color.mlx_color = 0xffffffff;
 	// }
+	if (i[0][2] != -1 && i[1][2] != -1 && i[2][2] != -1)
+	{
+		ksx_obj_add_vnrm(p_object, 3);
+		p_object->pp_tri[*p_idx]->p_norm1 = p_object->pp_vnrm[i[0][2] - 1];
+		p_object->pp_tri[*p_idx]->p_norm2 = p_object->pp_vnrm[i[1][2] - 1];
+		p_object->pp_tri[*p_idx]->p_norm3 = p_object->pp_vnrm[i[2][2] - 1];
+	}
 	(*p_idx)++;
 	p_object->size_tri = *p_idx;
 	return (p_object->pp_tri[*p_idx - 1]);
 }
 
-static int	get_f_split(char *p_str)
+static void	get_f_split(char *p_str, int *p_i)
 {
 	char	**pp_str;
-	int		i;
 
+	p_i[0] = -1;
+	p_i[1] = -1;
+	p_i[2] = -1;
 	if (!p_str)
-		return (-1);
+		return ;
 	pp_str = ft_split(p_str, '/');
 	if (!is_i_number(pp_str[0]))
-		return (ft_pparrclear((void **)pp_str), -1);
-	i = ft_atoi(pp_str[0]);
-	return (ft_pparrclear((void **)pp_str), i);
+		return (ft_pparrclear((void **)pp_str));
+	p_i[0] = ft_atoi(pp_str[0]);
+	if (pp_str[1] && is_i_number(pp_str[1]))
+		p_i[1] = ft_atoi(pp_str[1]);
+	if (pp_str[1] && pp_str[2] && is_i_number(pp_str[2]))
+		p_i[2] = ft_atoi(pp_str[2]);
+	return (ft_pparrclear((void **)pp_str));
 }
 
 t_vertex	*get_vn_of(t_fline *p_fline, t_object *p_object, int *p_idx)
 {
-	(void) p_fline;
-	(void) p_object;
+	if (!p_fline->words[1] || !p_fline->words[2] || !p_fline->words[3])
+		return (printf("Wrong float number.\n"), NULL);
+	ksx_obj_add_vnrm(p_object, 1);
+	p_object->pp_vnrm[*p_idx]->op.x = ft_atof(p_fline->words[1]);
+	p_object->pp_vnrm[*p_idx]->op.y = ft_atof(p_fline->words[2]);
+	p_object->pp_vnrm[*p_idx]->op.z = ft_atof(p_fline->words[3]);
 	(*p_idx)++;
-	return (NULL);
+	return (p_object->pp_vnrm[*p_idx]);
 }
